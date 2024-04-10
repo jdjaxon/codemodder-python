0143bc0b253afc51870a295f6556f624cc75bf37
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2024-04-10 15:04:42 -0400

    Switched CLI to use Click instead of Argparse.

--- src/codemodder/codemodder.py
+++ src/codemodder/codemodder.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import DefaultDict, Sequence
 
 from codemodder import __version__, registry
-from codemodder.cli import parse_args
+from codemodder.cli import parse_args, CLIArgs
 from codemodder.code_directory import match_files
 from codemodder.codemods.api import BaseCodemod
 from codemodder.codemods.semgrep import SemgrepRuleDetector
@@ -142,7 +142,7 @@ def run(original_args) -> int:
     codemod_registry = registry.load_registered_codemods()
 
     # A little awkward, but we need the codemod registry in order to validate potential arguments
-    argv = parse_args(original_args, codemod_registry)
+    argv: CLIArgs = parse_args(original_args, obj={'codemod_registry': codemod_registry})
     if not os.path.exists(argv.directory):
         logger.error(
             "given directory '%s' doesn't exist or can’t be read",
